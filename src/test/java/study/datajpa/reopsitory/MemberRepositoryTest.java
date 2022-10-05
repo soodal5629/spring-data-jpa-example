3b0cccfdde6e9c8c044651f548fa5fb52e2333a1
chaelynjang <chaelynjang@lotte.net>
2022-10-05 21:25:54 +0900

    기초 test

--- src/test/java/study/datajpa/reopsitory/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/reopsitory/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package study.datajpa.reopsitory;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,22 +11,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
-class MemberJpaRepositoryTest {
-    @Autowired MemberJpaRepository memberJpaRepository;
+class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
 
     @Test
-    void save() {
+    public void testSave(){
         Member member = new Member("memberA");
-        Member savedMember = memberJpaRepository.save(member);
-        Member findMember = memberJpaRepository.find(savedMember.getId());
+        Member savedMember = memberRepository.save(member);
+        // Optional을 get 으로 가져오는 것은 좋은 방법은 아니지만 지금은 연습이니까..
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
         assertThat(findMember.getId()).isEqualTo(savedMember.getId());
         assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
         // 같은 트랜잭션 내에서는 동일성 보장 -> true
         // 즉, 트랜잭션 다르면 다른 객체가 조회됨
         assertThat(findMember).isEqualTo(member);
-    }
-
-    @Test
-    void find() {
     }
 }
