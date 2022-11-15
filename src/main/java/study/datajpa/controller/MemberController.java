7eda647287e56f07d09f5ed2c5a172a89a446bb5
chaelynjang <chaelynjang@lotte.net>
2022-11-15 21:17:45 +0900

    페이징 처리

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.reopsitory.MemberRepository;
 
@@ -24,6 +28,14 @@ public class MemberController {
     @GetMapping("/members2/{id}")
     public String findMember2(@PathVariable("id") Member member) {
         return member.getUsername();
+    }
+
+    @GetMapping("/members")
+    public Page<Member> list(@PageableDefault(size = 5, sort = "username") Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        // 실제로는 엔티티 말고 DTO 로 리턴해줘야 함!!!
+        Page<MemberDto> result = page.map(MemberDto::new); // 생성자 이용한 코드
+        return page;
     }
 
     @PostConstruct
